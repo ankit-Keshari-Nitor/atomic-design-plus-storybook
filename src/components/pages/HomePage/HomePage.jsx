5e1a6af34ada3fb634344d1d85267fa1a9873c35
Ankit Keshari <ankit.keshari@nitorinfotech.com>
2024-01-24 19:17:24 +0530

    First Commit of atomic design pattern project

--- src/components/pages/HomePage/HomePage.jsx
+++ src/components/pages/HomePage/HomePage.jsx
@@ -1,72 +1,70 @@
-import React from 'react'
-import { HomeComponent } from '../../templates/Home/Home.stories'
+import React from "react"
+import { HomeComponent } from "../../templates/Home/Home.stories"
 
 function HomePage() {
-    const data = [
-            {
-                title: 'Like',
-                btnText: 'Subscribe'
-            },
-            {
-                title: 'Share',
-                btnText: 'Subscribe'
-            },
-            {
-              title: 'Like',
-              btnText: 'Subscribe'
-          },
-          {
-              title: 'Share',
-              btnText: 'Subscribe'
-          },
-          {
-            title: 'Like',
-            btnText: 'Subscribe'
-        },
-        {
-            title: 'Share',
-            btnText: 'Subscribe'
-        },
-        {
-          title: 'Like',
-          btnText: 'Subscribe'
-      },
-      {
-          title: 'Share',
-          btnText: 'Subscribe'
-      },
-      {
-        title: 'Like',
-        btnText: 'Subscribe'
+  const data = [
+    {
+      title: "Like",
+      btnText: "Love me",
     },
     {
-        title: 'Share',
-        btnText: 'Subscribe'
+      title: "Share",
+      btnText: "Love me",
     },
     {
-      title: 'Like',
-      btnText: 'Subscribe'
+      title: "Like",
+      btnText: "Love me",
     },
     {
-      title: 'Share',
-      btnText: 'Subscribe'
+      title: "Share",
+      btnText: "Love me",
     },
     {
-      title: 'Share',
-      btnText: 'Subscribe'
+      title: "Like",
+      btnText: "Love me",
     },
     {
-      title: 'Like',
-      btnText: 'Subscribe'
+      title: "Share",
+      btnText: "Love me",
     },
     {
-      title: 'Share',
-      btnText: 'Subscribe'
-    }
+      title: "Like",
+      btnText: "Love me",
+    },
+    {
+      title: "Share",
+      btnText: "Love me",
+    },
+    {
+      title: "Like",
+      btnText: "Love me",
+    },
+    {
+      title: "Share",
+      btnText: "Love me",
+    },
+    {
+      title: "Like",
+      btnText: "Love me",
+    },
+    {
+      title: "Share",
+      btnText: "Love me",
+    },
+    {
+      title: "Share",
+      btnText: "Love me",
+    },
+    {
+      title: "Like",
+      btnText: "Love me",
+    },
+    {
+      title: "Share",
+      btnText: "Love me",
+    },
   ]
-  return (
-      <HomeComponent cards={data}/>
-  )
+  return <HomeComponent cards={data} />
 }
 
 export default HomePage
